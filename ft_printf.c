230f35edb535971dfd6fc4164268ebb3e153fe6c
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-09-12 00:01:16 +0700

    another

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 23:15:30 by smeethon          #+#    #+#             */
-/*   Updated: 2022/09/11 23:57:38 by smeethon         ###   ########.fr       */
+/*   Updated: 2022/09/12 00:01:08 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@ int	ft_printf(const char *str, ...)
 {
 	va_list	x;
 	int		y;
-	int		z;
 
 	va_start(x, str);
-	while (str[z] != '\0')
+	while (*str)
 	{
 		if (str[z] == '%')
 		{
-			y += check2 (x, str[z + 1]);
-			z++;
+			y += check2 (x, *(str + 1));
+			str++;
 		}
 		else
-			y += write2 (str[z]);
-		z++;
+			y += write2 (*str);
+		str++;
 	}
 	va_end(x);
+	return (y);
 }
 
 int	ft_check(va_list ptr_arg, const char str)
